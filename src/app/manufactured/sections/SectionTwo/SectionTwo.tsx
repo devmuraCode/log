4e6e9e19d0a853acb16b6b610c158b3250ae30c0
muradevCode <muradkusherbaev@gmail.com>
2024-12-30 16:48:57 +0500

    sd

--- src/app/manufactured/sections/SectionTwo/SectionTwo.tsx
+++ src/app/manufactured/sections/SectionTwo/SectionTwo.tsx
@@ -10,76 +10,80 @@ import paper from "@/assets/paper.svg";
 import icon from "@/assets/icon-park-outline_.svg";
 import boxes from "@/assets/boxes.svg";
 import support from "@/assets/support.svg";
+import gra from '@/assets/garizont.svg'
 export const SectionTwo = () => {
   return (
     <div className={styles.wrapper}>
       <Container>
         <h1>Процесс организации производства</h1>
-        <div className={styles.grid}>
-          <div className={styles.step}>
-            <div className={styles.icon}>
-              <Image src={marketeq_research} alt="marketeq_research" />
-              <h2>1</h2>
+        <div className={styles.gridWrapper}>
+          <Image src={gra} alt="boxes" className={styles.gra} />
+          <div className={styles.grid}>
+            <div className={styles.step}>
+              <div className={styles.icon}>
+                <Image src={marketeq_research} alt="marketeq_research" />
+                <h2>1</h2>
+              </div>
+              <p>Тщательный анализ технического задания</p>
             </div>
-            <p>Тщательный анализ технического задания</p>
-          </div>
-          <div className={styles.step}>
-            <div className={styles.icon}>
-              <Image src={mage_id} alt="marketeq_research" />
-              <h2>2</h2>
+            <div className={styles.step}>
+              <div className={styles.icon}>
+                <Image src={mage_id} alt="marketeq_research" />
+                <h2>2</h2>
+              </div>
+              <p>Поиск подходящих исполнителей для выполнения задачи</p>
             </div>
-            <p>Поиск подходящих исполнителей для выполнения задачи</p>
-          </div>
-          <div className={styles.step}>
-            <div className={styles.icon}>
-              <Image src={outline_change} alt="marketeq_research" />
-              <h2>3</h2>
+            <div className={styles.step}>
+              <div className={styles.icon}>
+                <Image src={outline_change} alt="marketeq_research" />
+                <h2>3</h2>
+              </div>
+              <p>Сопоставление нескольких предложений и выбор оптимального</p>
             </div>
-            <p>Сопоставление нескольких предложений и выбор оптимального</p>
-          </div>
-          <div className={styles.step}>
-            <div className={styles.icon}>
-              <Image src={solar_file} alt="marketeq_research" />
-              <h2>6</h2>
+            <div className={styles.step}>
+              <div className={styles.icon}>
+                <Image src={solar_file} alt="marketeq_research" />
+                <h2>6</h2>
+              </div>
+              <p>Выполнение выходного аудита</p>
             </div>
-            <p>Выполнение выходного аудита</p>
-          </div>
-          <div className={styles.step}>
-            <div className={styles.icon}>
-              <Image src={transport} alt="marketeq_research" />
-              <h2>5</h2>
+            <div className={styles.step}>
+              <div className={styles.icon}>
+                <Image src={transport} alt="marketeq_research" />
+                <h2>5</h2>
+              </div>
+              <p>Предоставление транспортно-экспедиторской услуги</p>
             </div>
-            <p>Предоставление транспортно-экспедиторской услуги</p>
-          </div>
-          <div className={styles.step}>
-            <div className={styles.icon}>
-              <Image src={paper} alt="marketeq_research" />
-              <h2>4</h2>
+            <div className={styles.step}>
+              <div className={styles.icon}>
+                <Image src={paper} alt="marketeq_research" />
+                <h2>4</h2>
+              </div>
+              <p>Окончательное согласование технического задания</p>
             </div>
-            <p>Окончательное согласование технического задания</p>
-          </div>
-          <div className={styles.step}>
-            <div className={styles.icon}>
-              <Image src={icon} alt="marketeq_research" />
-              <h2>7</h2>
+            <div className={styles.step}>
+              <div className={styles.icon}>
+                <Image src={icon} alt="marketeq_research" />
+                <h2>7</h2>
+              </div>
+              <p>
+                Проверка соответствия стандартам качества на этапе производства
+              </p>
             </div>
-            <p>
-              Проверка соответствия стандартам качества на этапе производства
-            </p>
-          </div>
-          <div className={styles.step}>
-            <div className={styles.icon}>
-              <Image src={boxes} alt="marketeq_research" />
-              <h2>8</h2>
+            <div className={styles.step}>
+              <div className={styles.icon}>
+                <Image src={boxes} alt="marketeq_research" />
+                <h2>8</h2>
+              </div>
+              <p>Подготовка и запуск производства первой партии товара</p>
             </div>
-            <p>Подготовка и запуск производства первой партии товара</p>
-          </div>
-          <div className={styles.step}>
-            <div className={styles.icon}>
-              <Image src={support} alt="marketeq_research" />
-              <h2>9</h2>
+            <div className={styles.step}>
+              <div className={styles.icon}>
+                <Image src={support} alt="marketeq_research" />
+                <h2>9</h2>
+              </div>
+              <p>Обслуживание и консультационная поддержка</p>
             </div>
-            <p>Обслуживание и консультационная поддержка</p>
           </div>
         </div>
       </Container>
