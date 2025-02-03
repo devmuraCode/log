3d7baa79462d26e16cec4c3720297b88fb0c0ff6
KamilCodeDev <akadel.76@gmail.com>
2025-02-03 15:37:36 +0500

    telegrem

--- src/components/Modal/ContactModal.tsx
+++ src/components/Modal/ContactModal.tsx
@@ -1,10 +1,11 @@
 "use client";
 import Heading from "@/containers/Heading/Heading";
 import Input from "../Input/Input";
-import { useForm } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
 import Modal from "./Modal";
 import { useState } from "react";
 import useContactModal from "@/hooks/useContactModal";
+import axios from "axios";
 
 type Inputs = {
   email: string;
@@ -14,6 +15,8 @@ type Inputs = {
   description: string;
 };
 
+
+
 export const ContactModal = () => {
   const contactModal = useContactModal();
   const [isLoading, setIsLoading] = useState(false);
@@ -21,11 +24,34 @@ export const ContactModal = () => {
     register,
     handleSubmit,
     formState: { errors },
+    reset,
   } = useForm<Inputs>();
+  const [open, setOpen] = useState(false)
 
-  const onSubmit = (data: Inputs) => {
+
+  const onSubmit = async (data: Inputs) => {
     console.log(data);
+    await axios.post(`https://api.telegram.org/bot${process.env.NEXT_PUBLIC_TOKEN}/sendMessage`, {
+      chat_id: process.env.NEXT_PUBLIC_CHAT,
+      text: `Name: ${data.name}\nPhone: ${data.phone}\nEmail: ${data.email}\n Selectservice: ${data.selectservice}`,
+    } );
+
+    reset();
+    setOpen(false);
   };
+
+  const handleClickOpen = () =>{
+    setOpen(true);
+  };
+  const handleClose = () =>{
+    setOpen(false)
+    
+  }
+
+
+
+
+
 
   const bodyContent = (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
@@ -65,16 +91,15 @@ export const ContactModal = () => {
             required: "Пожалуйста, выберите услугу",
           })}
           disabled={isLoading}
-          className={`peer w-full p-4 pt-6 font-light bg-slate-50 border-0 rounded-md outline-none transition disabled:opacity-70 disabled:cursor-not-allowed ${
-            errors.selectservice ? "border-red-500" : "border-gray-300"
-          }`}
+          className={`peer w-full p-4 pt-6 font-light bg-slate-50 border-0 rounded-md outline-none transition disabled:opacity-70 disabled:cursor-not-allowed ${errors.selectservice ? "border-red-500" : "border-gray-300"
+            }`}
         >
           <option value="" disabled hidden>
             Выберите услугу...
           </option>
-          <option value="consultation">Консультация</option>
-          <option value="installation">Установка</option>
-          <option value="repair">Ремонт</option>
+          <option value="Консультация ">Консультация</option>
+          <option value="Установка">Установка</option>
+          <option value="Ремонт">Ремонт</option>
         </select>
         {errors.selectservice && (
           <p className="text-red-500 text-sm mt-1">
